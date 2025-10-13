d35fe96160493c382f3ffee0721a1245448041d0
Sahil Madaan <madaan.sahil27@gmail.com>
2025-10-13 18:46:20 +0530

    userRoutes, userController, userModel setup

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,11 @@ import cookieParser from "cookie-parser";
 
 // utils
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
 
 // Load env firjst
 dotenv.config();
@@ -21,8 +26,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World 😘");
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/upload", uploadRoutes);
+// app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
