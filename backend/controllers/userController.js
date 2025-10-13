e080cec6e0a805c8bea5739cd18c5e68c8e382d0
Sahil Madaan <madaan.sahil27@gmail.com>
2025-10-13 21:45:18 +0530

    orderModel handled

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,10 @@
-import User from '../models/userModel.js';
+import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
+    const { username, email, password, isAdmin } = req.body;
 
     if (!username || !email || !password) {
         throw new Error("Please fill all the inputs.");
@@ -15,7 +15,7 @@ const createUser = asyncHandler(async (req, res) => {
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = new User({ username, email, password: hashedPassword });
+    const newUser = new User({ username, email, password: hashedPassword, isAdmin: isAdmin ?? false });
 
     try {
         await newUser.save();
